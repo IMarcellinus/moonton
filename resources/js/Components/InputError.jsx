7210bbfce536c8c6b895d4785a6b7acc68cc27f3
Marcellinus <ignacymischa@gmail.com>
2022-08-26 18:07:45 +0700

    feat: integration sign up

--- resources/js/Components/InputError.jsx
+++ resources/js/Components/InputError.jsx
@@ -1,3 +1,3 @@
 export default function InputError({ message, className = '' }) {
-    return message ? <p className={'text-sm text-red-600 ' + className}>{message}</p> : null;
+    return message ? <p className={'mt-5 px-4 py-2 border-white border-2 rounded-mdtext-sm text-red-600 ' + className}>{message}</p> : null;
 }
