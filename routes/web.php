3e1dcad54931c995bcb53599c2fa2ea03981f28c
Marcellinus <ignacymischa@gmail.com>
2022-09-23 16:16:08 +0700

    feat: deleted and restore movie

--- routes/web.php
+++ routes/web.php
@@ -60,6 +60,7 @@ Route::prefix('prototype')->name('prototype.')->group(function () {
 });
 
 Route::middleware(['auth', 'role:admin'])->prefix('admin')->name('admin.dashboard.')->group(function () {
+    Route::put('movie/{movie}/restore', [AdminMovieController::class, 'restore'])->name('movie.restore');
     Route::resource('movie', AdminMovieController::class);
 });
 
